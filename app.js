70904ee4062cb0ae17548acecfd8042295b0cd93
TanvirAmi <tanvir.focus@gmail.com>
2022-04-08 16:38:07 +0600

    Added next and previous and random slide functionality

--- app.js
+++ app.js
@@ -37,10 +37,10 @@ const job = document.getElementById("job");
 const info = document.getElementById("info");
 
 const prevBtn = document.querySelector(".prev-btn");
-const nextvBtn = document.querySelector(".next-btn");
+const nextBtn = document.querySelector(".next-btn");
 const randomBtn = document.querySelector(".random-btn");
 
-let currentItem = 2;
+let currentItem = 0;
 
 //start loading the initial item
 window.addEventListener("DOMContentLoaded", function(){
@@ -55,3 +55,28 @@ function showPerson(person){
   job.textContent = item.job;
   info.textContent = item.text;
 }
+
+// show next person
+nextBtn.addEventListener("click", function(){
+  currentItem++;
+  if( currentItem > reviews.length - 1 ){
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
+
+//show previous person
+prevBtn.addEventListener("click", function(){
+  currentItem--;
+  if(currentItem < 0){
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+})
+
+// show random person
+randomBtn.addEventListener("click", function(){
+  currentItem = Math.floor(Math.random() * reviews.length);
+  //console.log(currentItem);
+  showPerson(currentItem);
+});
